1947240068600b8b150129334d0cd98204508f95
kurotu <kurotu@gmail.com>
2022-07-22 00:21:17 +0900

    Add build workflow

--- Editor/MaterialReplacer.cs
+++ Editor/MaterialReplacer.cs
@@ -1,4 +1,7 @@
-﻿namespace KRT.MaterialReplacer
+﻿using System.IO;
+using UnityEditor;
+
+namespace KRT.MaterialReplacer
 {
     /// <summary>
     /// Main class of MaterialReplacer.
@@ -9,5 +12,14 @@
         /// Version of MaterialReplacer.
         /// </summary>
         public static readonly string Version = "0.0.0";
+
+        private const string PackageJsonGUID = "1560de9bb4191fa478b78a2d79be403a";
+
+        private static string AssetRoot => Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(PackageJsonGUID));
+
+        private static void Export()
+        {
+            AssetDatabase.ExportPackage(AssetRoot, "MaterialReplacer.unitypackage", ExportPackageOptions.Recurse);
+        }
     }
 }
