252033d0fbf4d3e8a897ca4bd20fb1a16a4a47a3
kurotu <kurotu@gmail.com>
2022-07-23 03:31:26 +0900

    Implement version check

--- Editor/MaterialReplacer.cs
+++ Editor/MaterialReplacer.cs
@@ -1,5 +1,9 @@
-﻿using System.IO;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 using UnityEditor;
+using UnityEngine;
 
 namespace KRT.MaterialReplacer
 {
@@ -13,13 +17,83 @@ namespace KRT.MaterialReplacer
         /// </summary>
         public static readonly string Version = "0.0.0";
 
+        /// <summary>
+        /// Internal logger.
+        /// </summary>
+        internal static readonly ILogger Logger = new Logger(new LogHandler());
+
+        /// <summary>
+        /// Latest release info.
+        /// </summary>
+        internal static GitHubRelease latestRelease = null;
+
         private const string PackageJsonGUID = "1560de9bb4191fa478b78a2d79be403a";
+
+        private const string Repository = "kurotu/MaterialReplacer";
+
+        private const string Tag = "MaterialReplacer";
+
+        private static readonly HttpClient Client = new HttpClient();
+
+        static MaterialReplacer()
+        {
+            var isRelease = true;
+            if (isRelease)
+            {
+                Logger.filterLogType = LogType.Warning;
+            }
+
+            Client.Timeout = TimeSpan.FromSeconds(10);
+            Client.DefaultRequestHeaders.Add("User-Agent", $"MaterialReplacer-{Version}");
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    latestRelease = await GetLatestRelease();
+                }
+                catch (Exception e)
+                {
+                    Logger.LogException(e);
+                    latestRelease = null;
+                }
+            });
+        }
 
         private static string AssetRoot => Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(PackageJsonGUID));
 
         private static void Export()
         {
             AssetDatabase.ExportPackage(AssetRoot, "MaterialReplacer.unitypackage", ExportPackageOptions.Recurse);
+        }
+
+        private static async Task<GitHubRelease> GetLatestRelease()
+        {
+            var url = $"https://api.github.com/repos/{Repository}/releases/latest";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            var response = await Client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                Logger.LogError(Tag, $"Failed {request.Method} {request.RequestUri}: {(int)response.StatusCode} {response.ReasonPhrase}");
+                return null;
+            }
+            var body = await response.Content.ReadAsStringAsync();
+            var release = JsonUtility.FromJson<GitHubRelease>(body);
+            return release;
+        }
+
+        private class LogHandler : ILogHandler
+        {
+            public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
+            {
+                Debug.unityLogger.logHandler.LogFormat(logType, context, format, args);
+            }
+
+            public void LogException(Exception exception, UnityEngine.Object context)
+            {
+                Debug.unityLogger.LogException(exception, context);
+            }
         }
     }
 }
---
+++ Editor/SemVer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using UnityEngine;
+
+namespace KRT.MaterialReplacer
+{
+    /// <summary>
+    /// Represents semantic versioning.
+    /// </summary>
+    [Serializable]
+    internal class SemVer
+    {
+        [SerializeField]
+        private readonly int major;
+        [SerializeField]
+        private readonly int minor;
+        [SerializeField]
+        private readonly int patch;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemVer"/> class.
+        /// </summary>
+        /// <param name="version">Version string.</param>
+        internal SemVer(string version)
+        {
+            var part = version.TrimStart('v').Split('-');
+            var split = part[0].Split('.');
+            major = int.Parse(split[0]);
+            minor = int.Parse(split[1]);
+            patch = int.Parse(split[2]);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SemVer"/> class.
+        /// </summary>
+        /// <param name="major">Major.</param>
+        /// <param name="minor">Minor.</param>
+        /// <param name="patch">Patch.</param>
+        internal SemVer(int major, int minor, int patch)
+        {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch;
+        }
+
+        public static bool operator >(SemVer a, SemVer b)
+        {
+            if (a.major > b.major)
+            {
+                return true;
+            }
+
+            if (a.major == b.major && a.minor > b.minor)
+            {
+                return true;
+            }
+
+            if (a.major == b.major && a.minor == b.minor && a.patch > b.patch)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator <(SemVer a, SemVer b)
+        {
+            if (a.major < b.major)
+            {
+                return true;
+            }
+
+            if (a.major == b.major && a.minor < b.minor)
+            {
+                return true;
+            }
+
+            if (a.major == b.major && a.minor == b.minor && a.patch < b.patch)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator >=(SemVer a, SemVer b)
+        {
+            if (a > b)
+            {
+                return true;
+            }
+            return a.HasSameVersion(b);
+        }
+
+        public static bool operator <=(SemVer a, SemVer b)
+        {
+            if (a < b)
+            {
+                return true;
+            }
+            return a.HasSameVersion(b);
+        }
+
+        /// <summary>
+        /// Make semver string.
+        /// </summary>
+        /// <returns>"major.minor.patch".</returns>
+        public override string ToString()
+        {
+            return $"{major}.{minor}.{patch}";
+        }
+
+        /// <summary>
+        /// Gets whether the version is a major update against to old one.
+        /// </summary>
+        /// <param name="old">An old version to compare.</param>
+        /// <returns>true when the major version is larger than old one.</returns>
+        internal bool IsMajorUpdate(SemVer old)
+        {
+            if (this > old)
+            {
+                return major > old.major;
+            }
+
+            return false;
+        }
+
+        private bool HasSameVersion(SemVer a)
+        {
+            return ToString() == a.ToString();
+        }
+    }
+}
